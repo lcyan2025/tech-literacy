ecefc386b604be14080ccddef6465e5cd7c9400d
Roger Yen <jcyen.ntue@gmail.com>
2026-08-12 20:54:56 +0800

    fix: handle missing activity details

--- src/app/partner/[type]/[id]/page.tsx
+++ src/app/partner/[type]/[id]/page.tsx
@@ -9,15 +9,8 @@ import ExpandableImage from "@/components/expandable-image/expandable-image";
 const Page: React.FC = async ({ params }: any) => {
   const routeType = decodeURIComponent(params.type);
   const routeName = decodeURIComponent(params.id);
-  
-  const data: {
-    title: string;
-    description: string;
-    image: {
-      name: string;
-      url: string;
-    }[];
-  } = await findActivityData(routeName);
+
+  const data = await findActivityData(routeName);
 
   if (!data) {
     return notFound();
